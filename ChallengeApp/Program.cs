5f33d6c9cdb597404f24b0d2d4fddc50184d5f1d
Jan Tatarczuk <jantatarczuk@gmail.com>
2023-07-06 13:01:24 +0200

    zadanie domowe dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Eva";
+var name2 = "women";
+var myAge = 33;
+var myAge2 = 18;
+var myAge3 = 16;
+var myAge4 = 30;
+//1
+if (name2 == "women" &&  myAge4 < 50)
+{
+    Console.WriteLine("kobieta poniżej 30");
+}
+else
+{
+    Console.WriteLine("kobieta powyzej 30");
+}
+//2
+string result = name + myAge;
+Console.WriteLine(result);
+//3
+if (myAge2 > myAge3)
+{
+    Console.WriteLine("niepelnoletni mezczyzna");
+
+}
+else if (myAge2 < myAge)
+{
+    Console.WriteLine("pelnoletni mezczyzna");
+}
